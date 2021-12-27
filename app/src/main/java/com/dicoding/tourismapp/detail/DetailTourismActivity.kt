45f95149fd9dd5287d9f6471e14985680bb994a4
Andi Irham <Andi.irhamm@gmail.com>
2021-12-27 17:51:56 +0700

    apply modularization with hilt

--- app/src/main/java/com/dicoding/tourismapp/detail/DetailTourismActivity.kt
+++ app/src/main/java/com/dicoding/tourismapp/detail/DetailTourismActivity.kt
@@ -5,35 +5,30 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
-import com.dicoding.tourismapp.MyApplication
 import com.dicoding.tourismapp.R
-import com.dicoding.tourismapp.core.ui.ViewModelFactory
+import com.dicoding.tourismapp.core.domain.model.Tourism
 import com.dicoding.tourismapp.databinding.ActivityDetailTourismBinding
-import com.dicoding.tourismapp.domain.model.Tourism
-import javax.inject.Inject
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailTourismActivity : AppCompatActivity() {
 
     companion object {
         const val EXTRA_DATA = "extra_data"
     }
 
-    @Inject
-    lateinit var factory: ViewModelFactory
-
-    private val detailTourismViewModel: DetailTourismViewModel by viewModels {
-        factory
-    }
-
     private lateinit var binding: ActivityDetailTourismBinding
 
+    private val detailTourismViewModel: DetailTourismViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        (application as MyApplication).appComponent.inject(this)
         super.onCreate(savedInstanceState)
         binding = ActivityDetailTourismBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+//        hapus kode berikut
+//        val factory = ViewModelFactory.getInstance(this)
+//        detailTourismViewModel = ViewModelProvider(this, factory)[DetailTourismViewModel::class.java]
 
         val detailTourism = intent.getParcelableExtra<Tourism>(EXTRA_DATA)
         showDetailTourism(detailTourism)
